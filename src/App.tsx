10e82224f45dd477687b8e5b1443df01738889c8
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-02-01 12:35:39 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './components/assets/css/TodoList.css';
+import Todos from './components/Todo List/Todos';
+import AddTodo from './components/Todo List/AddTodo';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todos, setTodos] = useState<{ id: number, todo: string, completed: boolean }[]>([])
+  const handleAddTodo = (todo: string) => {
+    const newTodo = {
+      id: todos.length + 1,
+      todo,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo])
+  }
+
+  const handleDelete = (id: number) => {
+    const filteredTodo = todos.filter(todo => {
+      return todo.id !== id
+    })
+
+    setTodos(filteredTodo)
+  }
+
+  const toggleComplete = (id: number) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    )
+  }
+
+  const clearTodo = () => {
+    setTodos([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <p style={titleStyle}>Todo list</p>
+      <AddTodo addNewTodo={handleAddTodo} />
+      <div className='mini-header'>
+        <div className="number">You have {todos.length} todos</div>
+        <button className="btnClear" onClick={clearTodo}>Clear all</button>
+      </div>
+      {todos.length ? <Todos todos={todos} handleDelete={handleDelete} toggleComplete={toggleComplete} /> : <div className='no-todo'>No todo planned!</div>}
     </div>
   );
+}
+
+const titleStyle = {
+  fontSize: "20px",
+  fontWeight: "bold",
 }
 
 export default App;
